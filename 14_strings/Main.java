3f52337a03b7fe055ccfbe79dc040695009319b5
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-06-02 05:21:33 +0000

    string basic concepts added

---
+++ 14_strings/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String args[]) {
+        int num = 10;
+
+
+        
+        // String a = "Ankur";
+        // String b = "Ankur";
+
+        String a = "Ankur";
+        a = "Bhowmik";
+    }
+}
