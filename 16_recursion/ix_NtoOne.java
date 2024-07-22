35896d6e0e9f67b9af87538845c6d83f93249aa5
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-07-22 01:53:58 +0000

    new question added

---
+++ 16_recursion/ix_NtoOne.java
@@ -0,0 +1,14 @@
+public class Six_NtoOne {
+    public static void main(String[] args) {
+        print(5);
+    }
+
+    static void print (int n) {
+        if (n==0) {
+            return;
+        }
+
+        System.out.println(n);
+        print(n-1);
+    }
+}
