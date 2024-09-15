7d2088ae86b84dab45971403cdea25c595a1e82b
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-09-15 02:10:36 +0000

    new questions added

--- 16_recursion/TwentyTwo_subSeq.java
+++ 16_recursion/TwentyTwo_subSeq.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class TwentyTwo_subSeq {
     public static void main(String[] args) {
         String str = "abc";
-        System.out.println(subSeqRet("",str));
+       System.out.println(subSeqAsciiRet("",str));
+//        subSeqAscii("", "abc");
     }
     static void subSeq(String p, String up) {
         if(up.isEmpty()) {
@@ -29,5 +30,34 @@ public class TwentyTwo_subSeq {
 
         left.addAll(right);
         return left;
+    }
+
+    static void subSeqAscii(String p, String up) {
+        if(up.isEmpty()) {
+            System.out.println(p);
+            return ;
+        }
+
+        char ch = up.charAt(0);
+
+        subSeqAscii(p+ch, up.substring(1));
+        subSeqAscii(p, up.substring(1));
+        subSeqAscii(p+(ch+0),up.substring(1));
+    }
+
+    static ArrayList subSeqAsciiRet (String p, String up) {
+        if(up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> first = subSeqAsciiRet(p+ch, up.substring(1));
+        ArrayList<String> second = subSeqAsciiRet(p, up.substring(1));
+        ArrayList<String> third = subSeqAsciiRet(p + (ch+0), up.substring(1));
+
+        first.addAll(second);
+        first.addAll(third);
+        return first;
     }
 }
