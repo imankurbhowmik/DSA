e74e5c440d625b28d7be6dc23dd243ce16709e70
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-09-26 01:55:20 +0000

    new advanced questions added

--- 09_binarySearch/thirteen_countRotations.java
+++ 09_binarySearch/thirteen_countRotations.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-public class thirteen_countRotations {
+public class Thirteen_countRotations {
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        System.out.println(countRotations2(arr));
+        int[] arr = {1, 2, 2, 2, 0, 0, 0, 1, 1};
+        System.out.println(countRotations2WithDuplicates(arr));
     }
 
     static int countRotations(int[] arr){
@@ -61,5 +61,47 @@ public class thirteen_countRotations {
                }end = mid-1;
             }
         }return minIndex;
+    }
+
+    public static int countRotations2WithDuplicates (int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        int minElement = Integer.MAX_VALUE;
+        int minIndex = -1;
+
+        while(start <= end) {
+            int mid = start + (end-start)/2 ;
+
+            if(arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                if(arr[start] < minElement) {
+                    minElement = arr[start];
+                    minIndex = start;
+                }
+                start++;
+                end--;
+                continue;
+            }
+
+            if(arr[start] < arr[end]) {
+                if(arr[start] < minElement) {
+                    minElement = arr[start];
+                    minIndex = start;
+                }
+                break;
+            }
+            if(arr[start] <= arr[mid]) {
+                if(arr[start] < minElement) {
+                    minElement = arr[start];
+                    minIndex = start;
+                }
+                start = mid+1;
+            } else{
+                if(arr[mid] < minElement) {
+                    minElement = arr[mid];
+                    minIndex = mid;
+                }
+                end = mid-1;
+            }
+        } return minIndex;
     }
 }
--- 09_binarySearch/twelve_minElementRotatedBS.java
+++ 09_binarySearch/twelve_minElementRotatedBS.java
@@ -1,7 +1,8 @@
 public class twelve_minElementRotatedBS {
     public static void main(String[] args) {
-        int[] arr = {5, 6, 8, 1, 2, 3};
-        System.out.println(findMin(arr));
+        int[] arr = {6, 6, 6, 1, 1, 1, 2, 4, 4, 4, 6};
+//        System.out.println(findMin(arr));
+        System.out.println(findMinWithDuplicates(arr));
     }
 
     public static int findMin (int[] arr) {
@@ -11,6 +12,17 @@ public class twelve_minElementRotatedBS {
 
         while(start <= end) {
             int mid = start + (end-start)/2 ;
+
+//            if(arr[start] == arr[mid] && arr[mid] == arr[end]) {
+//                start++;
+//                end--;
+//                continue;
+//            }
+
+            if(arr[start] <= arr[end]) {
+                ans = Integer.min(ans, arr[start]);
+                break;
+            }
              if(arr[start] <= arr[mid]) {
                  ans = Integer.min(ans, arr[start]);
                  start = mid+1;
@@ -18,6 +30,34 @@ public class twelve_minElementRotatedBS {
                  ans = Integer.min(arr[mid], ans);
                  end = mid-1;
              }
+        } return ans;
+    }
+    public static int findMinWithDuplicates (int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        int ans = Integer.MAX_VALUE;
+
+        while(start <= end) {
+            int mid = start + (end-start)/2 ;
+
+            if(arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                ans = Integer.min(ans, arr[start]);
+                start++;
+                end--;
+                continue;
+            }
+
+            if(arr[start] < arr[end]) {
+                ans = Integer.min(ans, arr[start]);
+                break;
+            }
+            if(arr[start] <= arr[mid]) {
+                ans = Integer.min(ans, arr[start]);
+                start = mid+1;
+            } else{
+                ans = Integer.min(arr[mid], ans);
+                end = mid-1;
+            }
         } return ans;
     }
 }
