4c1b37f677492a7e3d7dcb39400d75d4ad788846
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-10-08 03:41:11 +0000

    new concepts added

---
+++ 20_oops/One_constructor.java
@@ -0,0 +1,99 @@
+import java.util.Arrays;
+
+
+public class One_constructor {
+    public static void main(String[] args) {
+
+        // stores 5 roll nos
+        int[] numbers = new int[5];
+
+        // stores five names
+        String[] names = new String[5];
+
+        // data of 5 students
+        int[] rollnos = new int[5];
+        String[] naams = new String[5];
+        float[] marks = new float[5];
+
+
+        Student[] students = new Student[5];
+        System.out.println(Arrays.toString(students)); // null
+
+        Student Ankur;
+        Ankur = new Student(4, "Ankur", 86.4f);
+
+
+        Student random = new Student(Ankur);
+
+        Student random2 = new Student();
+        System.out.println(random2.naams);
+//        Ankur.rollnos = 64;
+//        Ankur.naams = "Ankur";
+//        Ankur.marks = 86.4f;
+
+//        Ankur.changeName("Babu");
+//        Ankur.greeting();
+
+
+        System.out.println(Ankur.rollnos);   // 0  //64
+        System.out.println(Ankur.naams);  // null   //Ankur
+        System.out.println(Ankur.marks);  // 0.0   //90.0
+
+        System.out.println(random.rollnos);   // 0  //64
+        System.out.println(random.naams);  // null   //Ankur
+        System.out.println(random.marks);
+
+        Student one = new Student();
+        Student two = one;
+
+        one.naams = "Govind";
+        System.out.println(two.naams);
+     }
+
+    // create a class
+    // for every single student
+
+}
+class Student{
+    int rollnos;
+    String naams;
+    float marks = 90.0f;
+
+
+    void changeName(String newName) {
+        naams = newName;
+    }
+
+    void greeting() {
+        System.out.println("Hello my name is" + naams);
+    }
+
+
+    Student (Student other) {
+        this.rollnos = other.rollnos;
+        this.naams = other.naams;
+        this.marks = other.marks;
+
+    }
+
+    // we need a way to add these values of above props object by object
+
+    Student() {
+//        this.naams = "Ankur";
+//        this.rollnos = 64;
+//        this.marks = 86.4f;
+
+
+        // this is how u call another constructor from a constructor
+        this(13, "kuku", 90.9f);
+    }
+
+
+    // Student arpit = new Student (56, "Arpit", 90.8f)
+    // here this will be replaced by arpit
+    Student(int roll, String name, float marks) {
+        this.rollnos = roll;
+        this.naams = name;
+        this.marks = marks;
+    }
+}
---
+++ 20_oops/Two_wrapper.java
@@ -0,0 +1,47 @@
+public class Two_wrapper {
+    public static void main(String[] args) {
+        Integer a = 10;
+
+        Integer b = new Integer(45);
+
+        swap(a, b);
+
+        System.out.println(a + " " + b);  // no swap Integer is a final class
+
+        final A Ankur = new A("Ankur");
+        Ankur.name = "Bhow";
+
+        // when a non primitive is final we cannot reasign
+//        Ankur = new A("other");
+
+        A obj;
+
+        for(int i=0; i<1000000; i++) {
+            obj = new A("new object");
+        }
+    }
+
+
+
+    public static void swap(Integer a, Integer b) {
+        Integer temp = a;
+        a = b;
+        b = temp;
+    }
+}
+
+class A {
+    final int num = 10;
+    String name ;
+
+    public A(String name) {
+        this.name = name;
+    }
+
+
+    /// this will be called on garbage
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Object is destroyed");
+    }
+}
