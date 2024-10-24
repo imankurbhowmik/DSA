5f5ac4912fc7bd6cb42f53e7c579c1ed34e2af07
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-10-24 10:15:43 +0000

    new question added

--- 21_linkedLists/One_customLL.java
+++ 21_linkedLists/One_customLL.java
@@ -247,7 +247,30 @@ public class One_customLL {
 //
 //    }
 
-    
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        }while(slow != fast);
+
+        if(slow == 1) {
+            return true;
+        }return false;
+    }
+
+    private int findSquare(int number) {
+        int ans = 0;
+        while(number > 0) {
+            int rem = number % 10;
+            ans += rem * rem;
+            number/=10;
+        }return ans;
+    }
+
+
 
 
     public static void main(String[] args) {
