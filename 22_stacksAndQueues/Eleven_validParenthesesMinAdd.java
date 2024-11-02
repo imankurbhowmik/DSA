497f09c5e7e50a89401134435092ce14f0bb1e2a
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-11-02 15:19:42 +0000

    leetcode medium q added

---
+++ 22_stacksAndQueues/Eleven_validParenthesesMinAdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Eleven_validParenthesesMinAdd {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()) {
+            if(ch== ')') {
+                if(!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                }else{
+                    stack.push(ch);
+                }
+            }else{
+                stack.push(ch);
+            }
+        }return stack.size();
+    }
+}
