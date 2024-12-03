a14858a0df799164df4885074bea602c7bcb20e1
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-12-03 03:34:50 +0000

    new algos added

--- 23_trees/One_binaryTreesMain.java
+++ 23_trees/One_binaryTreesMain.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class One_binaryTreesMain {
     public static void main(String[] args) {
-        One_binaryTrees tree = new One_binaryTrees();
-        Scanner scanner = new Scanner(System.in);
-        tree.populate(scanner);
-        tree.prettyDisplay();
+//        One_binaryTrees tree = new One_binaryTrees();
+//        Scanner scanner = new Scanner(System.in);
+//        tree.populate(scanner);
+//        tree.prettyDisplay();
+
+        Two_BST binaryTree = new Two_BST();
+        int[] nums = {5, 10, 20, 5, 12};
+        binaryTree.populate(nums);
+        binaryTree.display();
+
     }
 }
--- 23_trees/Two_BST.java
+++ 23_trees/Two_BST.java
@@ -31,10 +31,6 @@ public class Two_BST {
         return root == null;
     }
 
-    public void display() {
-        display(root, "Root Node: ");
-    }
-
     public void insert(int value) {
         root = insert(value, root);
     }
@@ -58,14 +54,90 @@ public class Two_BST {
         return node;
     }
 
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
+        }
+    }
+
+    public void populatedSorted(int[] nums) {
+        populatedSorted(nums, 0, nums.length);
+    }
+
+    private void populatedSorted(int[] nums, int start, int end) {
+        if(start >= end) {
+            return;
+        }
+
+        int mid = start + (end-start)/2;
+
+        this.insert(nums[mid]);
+        populatedSorted(nums, start, mid);
+        populatedSorted(nums, mid+1, end);
+    }
+
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
+        if(node == null) {
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <=1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public void display() {
+        display(root, "Root Node: ");
+    }
+
     private void display(Node node, String details) {
         if(node == null) {
             return;
         }
         System.out.println(details + node.getValue());
-        display(node.left, "left side of " + node.getValue());
-        display(node.right, "Right side of " + node.getValue());
+        display(node.left, "left side of " + node.getValue() + ":");
+        display(node.right, "Right side of " + node.getValue() + ":");
     }
 
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+        System.out.println(node.value+ " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.value+ " ");
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value+ " ");
+    }
 
 }
