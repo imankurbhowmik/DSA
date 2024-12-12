fb13445b172c58b5074cc011f4bde37167be8abf
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-12-12 04:32:49 +0000

    BFS & DFS questions solved

--- 23_trees/Five_BFS.java
+++ 23_trees/Five_BFS.java
@@ -3,7 +3,7 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Five_levelOrderTraversal {
+public class Five_BFS {
 
     public static class TreeNode {
      int val;
@@ -222,5 +222,31 @@ public class Five_levelOrderTraversal {
             return n;
         }
         return findNode(node.right, x);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while(! queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if(left == null && right == null) {
+                continue;
+            }
+            if(left == null || right == null) {
+                return false;
+            }
+            if(left.val != right.val) {
+                return false;
+            }
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
     }
 }
