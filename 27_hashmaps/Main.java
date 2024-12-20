39e8b56eb14aa98404d915fc231f38fd61806aaf
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2024-12-20 04:43:41 +0000

    gettting hashcodes

---
+++ 27_hashmaps/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer a = 7875874;
+
+        String name = "Ankur";
+
+        int b = a.hashCode();
+        int c = name.hashCode();
+
+        System.out.println(b +" "+c);
+    }
+}
